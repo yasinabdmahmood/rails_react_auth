8ac5d3e4c93e8608f43b87f0a861e5b3e94849bd
yasinabdmahmood <abd181103@gmail.com>
2023-05-30 19:17:55 +0300

    Create SessionsController

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < Devise::SessionsController
+    respond_to :json
+  
+    def create
+      super do |user|
+        if request.format.json?
+          data = {
+            token: user.authentication_token,
+            email: user.email
+          }
+          render json: data, status: 201 and return
+        end
+      end
+    end
+  
+    # Add any additional custom actions or overrides for sessions as needed
+  end
+  
