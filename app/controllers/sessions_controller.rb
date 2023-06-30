fe750a08d530aacb4e61c6f596f1f1a977a0767e
yasinabdmahmood <abd181103@gmail.com>
2023-06-30 20:20:15 +0300

    Revert to initial commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,17 @@
 class SessionsController < Devise::SessionsController
-    respond_to :json
-  
-    def create
-      super do |user|
-        if request.format.json?
-          data = {
-            token: user.authentication_token,
-            email: user.email
-          }
-          render json: data, status: 201 and return
-        end
+  respond_to :json
+
+  def create
+    super do |user|
+      if request.format.json?
+        data = {
+          token: user.authentication_token,
+          email: user.email
+        }
+        render json: data, status: 201 and return
       end
     end
-  
-    # Add any additional custom actions or overrides for sessions as needed
   end
-  
+
+  # Add any additional custom actions or overrides for sessions as needed
+end
